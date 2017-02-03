e4711a18a53d30329d159e72e20fc07efd07232a
James Nearn <jnearn@jjkeller.com>
2017-02-03 15:20:46 -0500

    added more scripts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# osx-profile
+# README
+
+git clone https://github.com/jamesnearn/osx-profile.git
---
+++ update_android_sdk_platform_tools.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ~/Library/Android/sdk/platform-tools
+wget https://dl.google.com/android/repository/platform-tools-latest-darwin.zip

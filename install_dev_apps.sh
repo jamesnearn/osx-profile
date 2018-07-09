38011cbec8d9b02c03e539d24f3b38d3c583d844
James Nearn <jnearn@jjkeller.com>
2018-07-09 13:03:19 -0400

    added more apps

--- install_dev_apps.sh
+++ install_dev_apps.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+sudo xcodebuild -license
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install google-chrome
+
 brew cask install sqlitebrowser
 
+brew cask install slack
+
+brew cask install sourcetree
+
+brew cask install java
